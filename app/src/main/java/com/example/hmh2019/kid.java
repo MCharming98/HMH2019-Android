abba23f5dbb158995a5fca10d4b70f2548a63409
Yihui Zhu <whitehalfyi@berkeley.edu>
2019-03-23 16:09:37 -0700

    initialize 2 classes

---
+++ app/src/main/java/com/example/hmh2019/kid.java
@@ -0,0 +1,16 @@
+package com.example.hmh2019;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+public class kid{
+    string name; //The input string is the kid's name
+    public kid(string input){
+        name = input;
+    }
+}
